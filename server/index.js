9bca44729f9e18d59258356c8cb6937cfbc0b8da
Karthik Appiah <92764106+karthikappiah@users.noreply.github.com>
2021-11-17 06:43:28 -0800

    users CRD

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
+const server = express();
+server.use(express.json());
 
-app.use("/api/users", require("./routes/users"));
-
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-
-const PORT = 3001;
-app.listen(PORT, () => {
+PORT = 3001;
+server.listen(PORT, () => {
 	console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
+
+server.use("/api/users", require("./routes/users"));
